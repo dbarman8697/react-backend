267f011eb8cf5d7b7a84f6a7b0a3d2c04b7b80f2
Dwipayan Barman <dbarman8697@gmail.com>
2023-06-11 23:15:44 +0530

    updated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connection = mongoose.connect(
-  "mongodb+srv://dwipayan:dwipayan@cluster0.m53osop.mongodb.net/deployData"
+  "mongodb+srv://dwipayan:dwipayan@cluster0.k1vgwtf.mongodb.net/deployData"
 );
 
 module.exports = { connection };
